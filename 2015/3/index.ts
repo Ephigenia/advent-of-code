253c7759f71bbdba6ef4e5f72633f0974d117c63
Marcel Eichner <love@ephigenia.de>
2022-12-20 21:26:59 +0100

    2015-3

--- 2015/3/index.ts
+++ 2015/3/index.ts
@@ -7,13 +7,10 @@ enum DIRECTION {
 }
 
 class Player {
-  public visited: Map<string, number> = new Map();
   public pos: [number, number] = [0, 0];
   public moves: DIRECTION_STRING[] = [];
-                  // x1      y1      x2      y2
-  public borders: [number, number, number, number] = [0,0,0,0];
 
-  constructor(public name: string) {}
+  constructor(public world: World, public name: string) {}
 
   move(direction: DIRECTION_STRING) {
     switch(direction) {
@@ -31,27 +28,7 @@ class Player {
         break;
     };
     this.moves.push(direction);
-    this.visit(this.pos[0], this.pos[1]);
-  }
-
-  visit(x: number, y: number): void {
-    const key = [x,y].join(',');
-    this.visited.set(key, this.getVisitsAt(x, y) + 1);
-    this.extendBorders();
-  }
-
-  private extendBorders(): void {
-    // X
-    if (this.borders[0] > this.pos[0]) this.borders[0] = this.pos[0];
-    if (this.borders[2] < this.pos[0]) this.borders[2] = this.pos[0];
-    // Y
-    if (this.borders[1] > this.pos[1]) this.borders[1] = this.pos[1];
-    if (this.borders[3] < this.pos[1]) this.borders[3] = this.pos[1];
-  }
-
-  getVisitsAt(x: number, y: number): number {
-    const key = [x,y].join(',');
-    return this.visited.get(key) || 0;
+    this.world.visit(this.pos[0], this.pos[1]);
   }
 
   toString() {
@@ -60,37 +37,53 @@ class Player {
 }
 
 class World {
+  public visited: Map<string, number> = new Map();
+                   // x1      y1      x2      y2
+  public borders: [number, number, number, number] = [0,0,0,0];
 
   players = new Set<Player>();
 
   createPlayer(name: string) {
-    const player = new Player(name)
+    const player = new Player(this, name);
     this.players.add(player);
+    this.visit(0, 0);
     return player;
   }
 
-  getBorders() {
-    const players = [...this.players];
-    return [
-      Math.min(...players.map(p => p.borders[0])),
-      Math.min(...players.map(p => p.borders[1])),
-      Math.max(...players.map(p => p.borders[2])),
-      Math.max(...players.map(p => p.borders[3])),
-    ];
+  visit(x: number, y: number): void {
+    const key = [x,y].join(',');
+    const val = this.getVisitsAt(x, y) + 1;
+    this.visited.set(key, val);
+    this.extendBorders(x, y);
+  }
+
+  private extendBorders(x: number, y: number): void {
+    // x
+    if (this.borders[0] > x) this.borders[0] = x;
+    if (this.borders[2] < x) this.borders[2] = x;
+    // y
+    if (this.borders[1] > y) this.borders[1] = y;
+    if (this.borders[3] < y) this.borders[3] = y;
+  }
+
+  getVisitsAt(x: number, y: number): number {
+    const key = [x,y].join(',');
+    return this.visited.get(key) || 0;
   }
 
   render() {
-    const borders = this.getBorders();
     let str = '';
-    for(let y = borders[1]; y < borders[3]; y++) {
-      for(let x = borders[0]; x < borders[2]; x++) {
-        // const visits = this.getVisitsAt(x, y);
-        let char = '░';
-        // if (visits > 5) {
-        //   char = '▓';
-        // } else if (visits > 0) {
-        //   char = '▒';
-        // }
+    for (let y = this.borders[1]; y <= this.borders[3]; y++) {
+      for (let x = this.borders[0]; x <= this.borders[2]; x++) {
+        const visits = this.getVisitsAt(x, y);
+        let char = '.';
+        if (visits > 8) {
+          char = '▓';
+        } else if (visits > 4) {
+          char = '▒';
+        } else if (visits > 0) {
+          char = '░';
+        }
         str += char;
       }
       str += '\n';
@@ -101,10 +94,7 @@ class World {
 
 async function main(filename: string) {
   const inputFilename = resolve(__dirname, filename);
-  const raw = Array.from((await readFile(inputFilename)).toString().trim()).slice(0, 200);
-
-  // const raw = Array.from('^v');
-  // const raw = Array.from('^>v<<');
+  const raw = Array.from((await readFile(inputFilename)).toString().trim());
 
   const world = new World();
   const santa = world.createPlayer('santa');
@@ -117,14 +107,13 @@ async function main(filename: string) {
     }
   });
 
-  console.log(world.players);
-  console.log(world.getBorders());
-
+  console.log('total houses visited', world.visited.size);
+  console.log(world.borders);
   // console.log(world.visited.size);
   console.log(world.render());
 
-  // const onePresentVisits = [...world.visited].filter(([key, val]) => val >= 1);
-  // console.log('number of houses with at least one present: %d', onePresentVisits.length);
+  const onePresentVisits = [...world.visited].filter(([key, val]) => val >= 1);
+  console.log('number of houses with at least one present: %d', onePresentVisits.length);
 }
 
 main('input.txt');
