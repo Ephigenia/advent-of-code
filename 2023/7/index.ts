6eb77cf6404a3df44cb38b3b1355d0d36231cf4c
Marcel Eichner <love@ephigenia.de>
2023-12-08 19:17:58 +0100

    chore: 2023-7

--- 2023/7/index.ts
+++ 2023/7/index.ts
@@ -5,76 +5,53 @@ function parseInput(input: string) {
   return input.split('\n');
 }
 
-// relative strengs decreasing, A strongest, 2 weakest
-// A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, or 2
-
-const scoreMap = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'].reverse();
-
 function getScoreForCard(card: string) {
-  return scoreMap.indexOf(card) + 1;
+  const scoreMap = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'].reverse();
+  return scoreMap.indexOf(card) + 2;
 }
 
 class Card {
   public score = 0;
-  constructor(public symbol : string, public count: number = 0) {
+  constructor(public symbol : string, public count: number = 1) {
     this.score = getScoreForCard(symbol);
   }
 }
 
 class Hand {
+  sortedCards: Card[] = [];
   cards: Card[] = [];
   score: number = 0;
   constructor(cardSymbols: string[], public bid: number = 0) {
+    this.cards = cardSymbols.map((symbol) => new Card(symbol));
+
     cardSymbols.forEach((symbol) => {
-      const card = this.cards.find(card => card.symbol === symbol);
+      const card = this.sortedCards.find(card => card.symbol === symbol);
       if (card) card.count++;
-      else this.cards.push(new Card(symbol));
+      else this.sortedCards.push(new Card(symbol));
     });
-    this.cards = this.cards.sort((a, b) => b.count - a.count);
+    this.sortedCards = this.sortedCards.sort((a, b) => b.count - a.count);
 
-    // find the card with the most occurrences
-    const cardScore = this.cards.reduce((a, c) => a+c.score, 0);
-    if (this.cards.length === 1) {
-      this.score = cardScore * 20;
-    } else if (this.cards.length === 2) {
-      if (this.cards[0].count === 4) {
-        this.score = cardScore * 10;
-      } else if (this.cards[0].count === 3 && this.cards[1].count === 2) {
-        this.score = cardScore * 5;
-      }
-    } else if (this.cards.length === 3) {
-      this.score = cardScore * 3;
-    } else if (this.cards.length === 4) {
-      this.score = cardScore * 2;
-    } else if (this.cards.length === 5) {
-      this.score = cardScore;
+    if (this.sortedCards[0].count === 5) {
+      this.score = 6;
+    } else if (this.sortedCards[0].count === 4) {
+      this.score = 5;
+    } else if (this.sortedCards[0].count === 3 && this.sortedCards[1].count === 2) {
+      this.score = 4;
+    } else if (this.sortedCards[0].count === 3 && this.sortedCards[1].count === 1) {
+      this.score = 3;
+    } else if (this.sortedCards[0].count === 2 && this.sortedCards[1].count === 2) {
+      this.score = 2;
+    } else if (this.sortedCards[0].count === 2 && this.sortedCards[1].count === 1) {
+      this.score = 1;
+    } else {
+      this.score = 0;
     }
-  }
-
-  toString() {
-    return this.cards.map((card) => card.symbol.repeat(card.count+1)).join('');
   }
 }
 
 interface Card {
   symbol: string;
   count: number;
-}
-
-function getSameCards(symbol: string[]) {
-  const counter: Card[] = [];
-  symbol.forEach((symbol) => {
-    const card = counter.find(card => card.symbol === symbol);
-    if (card) card.count++;
-  });
-  console.log(counter);;
-}
-
-function getScoreHand(cards: string[]) {
-
-  getSameCards(cards)
-  const score = cards.reduce((acc, card) => acc + getScoreForCard(card), 0);
-  return score;
 }
 
 async function main(filename: string) {
@@ -88,11 +65,23 @@ async function main(filename: string) {
     const bid = parseInt(bidString, 10);
     return new Hand(cards, bid);
   });
-  // console.log(hands);
 
-  hands.forEach((hand, i) => {
-    console.log(i, hand.toString(), hand.score);
+  const sortedHands = hands.sort((a: Hand, b: Hand) => {
+    if (a.score > b.score) return 1;
+    if (a.score < b.score) return -1;
+    if (a.score === b.score) {
+      // both ranks are equal, compare hands card by card
+      for (let i = 0; i < a.cards.length; i++) {
+        if (a.cards[i].score > b.cards[i].score) return 1;
+        if (a.cards[i].score < b.cards[i].score) return -1;
+        continue;
+      }
+    }
+    return 0;
   });
+
+  const sumBids = sortedHands.reduce((a, hand, index) => a + hand.bid * (index + 1), 0);
+  console.log('sum', sumBids);
 }
 
 const INPUT_FILENAME = process.argv.pop() || 'input.txt';
