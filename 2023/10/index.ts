439e121a713f171802bfeb7cc4e24009160adfe7
Marcel Eichner <love@ephigenia.de>
2023-12-11 20:18:16 +0100

    chore: wip

--- 2023/10/index.ts
+++ 2023/10/index.ts
@@ -1,16 +1,47 @@
 import { readFile } from "fs/promises";
 import { resolve } from "path";
 
-function parseInput(input: string) {
-  return input.split('\n');
+
+class Surface {
+  public shapes: string[][] = [[]];
+  public width = 0;
+  public height = 0;
+
+  public constructor(lines: string) {
+    this.shapes = lines.split('\n').map(line => line.split(''));
+    this.width = lines[0].length - 1;
+    this.height = lines.length - 1;
+  }
+
+  public get(x: number, y: number): string {
+    return this.shapes[y][x];
+  }
+
+  static fromString(input: string): Surface {
+    const surface = new Surface(input);
+    return surface;
+  }
+
+  public toString(): string {
+    return this.shapes.map(row => row.join('')).join('\n')
+      .replace(/F/g, '┌')
+      .replace(/L/g, '└')
+      .replace(/7/g, '┐')
+      .replace(/J/g, '┘')
+      .replace(/\./g, ' ')
+      .replace(/-/g, '─');
+  }
 }
+
+
+
 
 async function main(filename: string) {
   const inputFilename = resolve(filename);
   const rawInput = (await readFile(inputFilename)).toString().trim();
-  const input = parseInput(rawInput)
 
-  console.log(input);
+  const surface = Surface.fromString(rawInput);
+  console.log(surface.toString());
 }
 
 const INPUT_FILENAME = process.argv.pop() || 'input.txt';
