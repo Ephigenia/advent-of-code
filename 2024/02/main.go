114771b2ae1703c4249ef5e3ea5bc3fdff7c9de9
Marcel Eichner <love@ephigenia.de>
2024-12-05 20:41:12 +0100

    chore: wip

---
+++ 2024/02/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"errors"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/Ephigenia/advent-of-code/2024/lib"
+	"github.com/davecgh/go-spew/spew"
+)
+
+func main() {
+	pwd, _ := os.Getwd()
+	args := os.Args
+
+	if len(args) < 2 {
+		lib.ExitWithError(errors.New("please provide a filename"))
+	}
+	filename := path.Join(pwd, args[1])
+
+	rawInput, err := lib.ReadInputFile(filename)
+	if err != nil {
+		lib.ExitWithError(err)
+	}
+
+	processInputPartOne(rawInput)
+}
+
+func processInputPartOne(input string) {
+	spew.Dump(strings.Split(input, "\n"))
+}
