5d91c3aaae4a8de34f3e09cf179373ed3cc2e6f4
Marcel Eichner <love@ephigenia.de>
2024-12-05 20:29:21 +0100

    chore: wip

--- 2024/01/main.go
+++ 2024/01/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"math"
 	"os"
 	"path"
 	"strings"
@@ -19,31 +21,36 @@ func main() {
 	}
 	filename := path.Join(pwd, args[1])
 
-	rawInput, err := ReadInputFile(filename)
+	rawInput, err := lib.ReadInputFile(filename)
 	if err != nil {
 		lib.ExitWithError(err)
 	}
 
-	processInput(rawInput)
+	processInputPartOne(rawInput)
 }
 
-func ReadInputFile(filename string) (string, error) {
-	// read the whole file
-	dat, err := os.ReadFile(filename)
-	if err != nil {
-		return "", nil
-	}
-	return strings.TrimRight(string(dat), "\n"), nil
-}
-
-func processInput(input string) {
+func processInputPartOne(input string) {
 	lines := strings.Split(input, "\n")
 	right := make([]int, len(lines))
 	left := make([]int, len(lines))
+
 	for i, line := range lines {
 		values := lib.ArrStrToInt(strings.Split(line, "   "))
 		left[i] = values[0]
 		right[i] = values[1]
 	}
-	spew.Dump(left, right)
+
+	// find min values of remaining array and calculate distance
+	sum := 0
+	for i := range left {
+		minLeft, minLeftIndex := lib.ArrMin(left)
+		minRight, minRightIndex := lib.ArrMin(right)
+		left = lib.ArrIntPopIndex(left, minLeftIndex)
+		right = lib.ArrIntPopIndex(right, minRightIndex)
+
+		dist := math.Abs(float64(minLeft - minRight))
+		fmt.Printf("#%d minLeft: %d, minRight: %d, dist: %f\n", i, minLeft, minRight, dist)
+		sum += int(dist)
+	}
+	spew.Dump(sum)
 }
--- 2024/lib/helper.go
+++ 2024/lib/helper.go
@@ -4,11 +4,50 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func ExitWithError(err error) {
 	fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 	os.Exit(1)
+}
+
+func ReadInputFile(filename string) (string, error) {
+	// read the whole file
+	dat, err := os.ReadFile(filename)
+	if err != nil {
+		return "", nil
+	}
+	return strings.TrimRight(string(dat), "\n"), nil
+}
+
+func ArrMin(input []int) (int, index int) {
+	min := input[0]
+	index = 0
+	for i, v := range input {
+		if v < min {
+			min = v
+			index = i
+		}
+	}
+	return min, index
+}
+
+func ArrMax(input []int) (int, index int) {
+	max := input[0]
+	index = 0
+	for i, v := range input {
+		if v > max {
+			max = v
+			index = i
+		}
+	}
+	return max, index
+}
+
+// remove on item of an array
+func ArrIntPopIndex(input []int, index int) []int {
+	return append(input[:index], input[index+1:]...)
 }
 
 func ArrStrToInt(input []string) []int {
