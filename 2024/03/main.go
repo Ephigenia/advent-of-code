61508a4f089902297af009185cfef16aa486e759
Marcel Eichner <love@ephigenia.de>
2024-12-08 20:07:29 +0100

    chore: wip

--- 2024/03/main.go
+++ 2024/03/main.go
@@ -8,6 +8,7 @@ import (
 	"regexp"
 
 	"github.com/Ephigenia/advent-of-code/2024/lib"
+	"github.com/davecgh/go-spew/spew"
 )
 
 func main() {
@@ -25,6 +26,8 @@ func main() {
 	}
 
 	processInputPartOne(rawInput)
+
+	processInputPartTwo(rawInput)
 }
 
 func processInputPartOne(rawInput string) {
@@ -35,6 +38,32 @@ func processInputPartOne(rawInput string) {
 		values := []string{match[1], match[2]}
 		factors := lib.ArrStrToInt(values)
 		sum += factors[0] * factors[1]
+	}
+
+	fmt.Printf("Result is %d\n", sum)
+}
+
+func processInputPartTwo(rawInput string) {
+	regex := regexp.MustCompile(`mul\((\d+),(\d+)\)|do\(\)|don't\(\)`)
+	matches := regex.FindAllStringSubmatch(rawInput, -1)
+	spew.Dump(matches)
+	sum := 0
+	active := true
+	for _, match := range matches {
+		command := match[0]
+		if command == "do()" {
+			active = true
+			continue
+		} else if command == "don't()" {
+			active = false
+			continue
+		} else if command[:3] == "mul" {
+			if active {
+				values := []string{match[1], match[2]}
+				factors := lib.ArrStrToInt(values)
+				sum += factors[0] * factors[1]
+			}
+		}
 	}
 
 	fmt.Printf("Result is %d\n", sum)
