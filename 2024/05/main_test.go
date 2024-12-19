c719c6df3bf4d1b7528a3d3a46610c74b546bceb
Marcel Eichner <love@ephigenia.de>
2024-12-19 20:53:23 +0100

    chore: wip

--- 2024/05/main_test.go
+++ 2024/05/main_test.go
@@ -7,6 +7,17 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestIsCorrectOrder(t *testing.T) {
+	orderingRules := [][]int{
+		{75, 47},
+		{75, 61},
+		{75, 53},
+		{75, 29},
+	}
+	result := IsCorrectOrder(75, orderingRules)
+	assert.Equal(t, result, true)
+}
+
 func TestCheckOrderOfPages(t *testing.T) {
 	fmt.Printf("asd")
 	pages := []int{75, 47, 61, 53, 29}
