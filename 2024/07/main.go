5c175f9ff667be136aef75274901c59285b43f76
Marcel Eichner <love@ephigenia.de>
2024-12-19 21:15:09 +0100

    chore: wip

--- 2024/07/main.go
+++ 2024/07/main.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 
 	"github.com/Ephigenia/advent-of-code/2024/lib"
+	"github.com/davecgh/go-spew/spew"
 )
 
 func main() {
@@ -53,10 +54,39 @@ func parseInput(str string) []InputLine {
 	return inputs
 }
 
-func processInputPartOne(inputs []InputLine) {
-	for _, input := range inputs {
-		fmt.Printf("%d: %v\n", input.result, input.numbers)
+func operate(a, b int, operation string) int {
+	if operation == "+" {
+		return a + b
+	}
+	if operation == "*" {
+		return a * b
+	}
+	panic("invalid operatoration")
+}
 
+func TryMe(result int, args ...int) int {
+	solutions := []int{}
+	for index, arg := range args {
+		if index == len(args)-1 {
+			continue
+		}
+		solution := operate(arg, args[index+1], "+")
+		solutions = append(solutions, solution)
+		solution = operate(arg, args[index+1], "*")
+		solutions = append(solutions, solution)
+	}
+	spew.Dump(solutions)
+	return 0
+}
+
+func processInputPartOne(inputs []InputLine) {
+	for i, input := range inputs {
+		if i > 1 {
+			continue
+		}
+		correctCombinations := TryMe(input.result, input.numbers...)
+
+		fmt.Printf("%d: %v %d\n", input.result, input.numbers, correctCombinations)
 	}
 	// spew.Dump(inputs)
 }
