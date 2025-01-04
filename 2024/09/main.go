5561fc5483038388bd7662564ff31bac030b4318
Marcel Eichner <love@ephigenia.de>
2025-01-04 20:38:06 +0100

    chore: wip

--- 2024/09/main.go
+++ 2024/09/main.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 
 	"github.com/Ephigenia/advent-of-code/2024/lib"
+	"github.com/davecgh/go-spew/spew"
 )
 
 func main() {
@@ -27,19 +28,62 @@ func main() {
 	input := parseInput(rawInput)
 	fmt.Printf("Input: %v\n", input)
 	printMap(input)
-	processInputPartOne(rawInput)
+	// processInputPartOne(input)
+}
+
+func parseInput(inputs string) []int {
+	all := []int{}
+	for i := 0; i < len(inputs); i++ {
+		c, _ := strconv.Atoi(string(inputs[i]))
+		all = append(all, c)
+	}
+	return all
+}
+
+type File struct {
+	id     int
+	blocks int
+	free   int
+}
+
+type Disk struct {
+	files []File
+}
+
+func (d *Disk) addFile(f File) {
+	d.files = append(d.files, f)
 }
 
 func printMap(m []int) {
+	disk := Disk{}
+
+	for i := 0; i < len(m); i++ {
+		if i%2 == 1 {
+			continue
+		}
+		fmt.Printf("i: %d\n", i)
+		free := 0
+		if i < len(m)-1 {
+			free = m[i+1]
+		}
+		disk.addFile(File{
+			id:     m[i] / 2,
+			blocks: m[i],
+			free:   free,
+		})
+	}
+
+	spew.Dump(disk)
+
 	mode := 1
 	parts := []string{}
 	id := 0
 	for i := 0; i < len(m); i++ {
 		part := ""
 		length := m[i]
-		if mode == 0 {
+		if mode == 0 { // free space
 			part = strings.Repeat(".", length)
-		} else {
+		} else { // blocked space
 			part = strings.Repeat(strconv.Itoa(id), length)
 		}
 		parts = append(parts, part)
@@ -51,18 +95,10 @@ func printMap(m []int) {
 		}
 	}
 	fmt.Printf("%s\n", strings.Join(parts, ""))
-}
-
-func parseInput(inputs string) []int {
-	all := []int{}
-	for i := 0; i < len(inputs); i++ {
-		c, _ := strconv.Atoi(string(inputs[i]))
-		all = append(all, c)
-		// fmt.Printf("%d\n", c)
-	}
-	return all
-}
-
-func processInputPartOne(inputs string) {
 
 }
+
+// func processInputPartOne(inputs []int) {
+// 	spew.Dump(inputs)
+// 	printMap(inputs)
+// }
