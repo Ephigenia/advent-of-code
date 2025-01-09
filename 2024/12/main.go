18e12072369de2403f809e3988c35f7b8f39d5b3
Marcel Eichner <love@ephigenia.de>
2025-01-09 20:39:02 +0100

    chore: wip

--- 2024/12/main.go
+++ 2024/12/main.go
@@ -25,44 +25,12 @@ func main() {
 	partOne(rawInput)
 }
 
-type Point struct {
-	x, y int
-}
-type Area struct {
-	points []Point
-}
-
-func (a Area) Width() int {
-	w := 0
-	for _, p := range a.points {
-		if p.x > w {
-			w = p.x
-		}
-	}
-	return w + 1
-}
-func (a Area) Height() int {
-	h := 0
-	for _, p := range a.points {
-		if p.y > h {
-			h = p.y
-		}
-	}
-	return h + 1
-}
-
 func partOne(input string) {
 	matrix := lib.NewStringMatrixFromString(input)
 	fmt.Printf("%s\n", matrix.String())
 
-	// parse all areas
-	area := Area{points: []Point{
-		{x: 0, y: 0},
-		{x: 0, y: 1},
-		{x: 1, y: 2},
-		{x: 1, y: 2},
-	}}
-
-	fmt.Printf("w/h: %d/%d\n", area.Width(), area.Height())
-
+	// areas := []lib.StringMatrix{}
+	matrix.ForEach(func(x, y int, val string) {
+		fmt.Printf("x/y: %d/%d\n", x, y)
+	})
 }
