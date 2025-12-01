8a60118dc17a29817e0036581541d92881cebc4e
Marcel Eichner <love@ephigenia.de>
2025-12-01 23:47:56 +0100

    chore: wip

--- 2025/01/main.go
+++ 2025/01/main.go
@@ -4,6 +4,9 @@ import (
 	"errors"
 	"os"
 	"path"
+	"regexp"
+	"strconv"
+	"strings"
 
 	"github.com/Ephigenia/advent-of-code/2025/lib"
 	"github.com/davecgh/go-spew/spew"
@@ -23,5 +26,45 @@ func main() {
 		lib.ExitWithError(err)
 	}
 
-	spew.Dump(rawInput)
+	processInputPartOne(rawInput)
+}
+
+type InputItem struct {
+	direction string
+	count     int
+}
+
+func validateInputLine(line string) error {
+	match, _ := regexp.MatchString("^[LR]\\d+$", line)
+	if !match {
+		return errors.New("line does not match expected format")
+	}
+
+	return nil
+}
+
+func convertInputLineToItem(line string) []InputItem {
+	if err := validateInputLine(line); err != nil {
+		panic(err)
+	}
+
+	value, _ := strconv.Atoi(line[1:])
+
+	return []InputItem{
+		{
+			direction: line[0:1],
+			count:     value,
+		},
+	}
+}
+
+func processInputPartOne(input string) {
+	lines := strings.Split(input, "\n")
+
+	items := []InputItem{}
+	for _, line := range lines {
+		newItems := convertInputLineToItem(line)
+		items = append(items, newItems...)
+	}
+	spew.Dump(items)
 }
