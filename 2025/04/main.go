751a44581b6d683930740e454165c174ffa33d6a
Marcel Eichner <love@ephigenia.de>
2025-12-22 20:25:52 +0100

    chore: Wip

---
+++ 2025/04/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path"
+
+	lib2024 "github.com/Ephigenia/advent-of-code/2024/lib"
+	lib2025 "github.com/Ephigenia/advent-of-code/2025/lib"
+)
+
+func main() {
+	pwd, _ := os.Getwd()
+	args := os.Args
+
+	if len(args) < 2 {
+		lib2024.ExitWithError(errors.New("please provide a filename"))
+	}
+	inputFilename := path.Join(pwd, args[1])
+
+	rawInput, err := lib2024.ReadInputFile(inputFilename)
+	if err != nil {
+		lib2024.ExitWithError(err)
+	}
+
+	processInputPartOne(rawInput)
+}
+
+// correct answer is 17179
+func processInputPartOne(input string) {
+	grid := lib2025.NewGridFromString(input)
+	grid.Print()
+	fmt.Println("Part One:", 0)
+}
---
+++ 2025/lib/grid.go
@@ -0,0 +1,53 @@
+package lib
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Grid struct {
+	Cells         [][]rune
+	Width, Height int
+}
+
+func NewGrid(width, height int) *Grid {
+	cells := make([][]rune, height)
+	for i := range cells {
+		cells[i] = make([]rune, width)
+	}
+	return &Grid{
+		Cells:  cells,
+		Width:  width,
+		Height: height,
+	}
+}
+
+func NewGridFromString(input string) *Grid {
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+	height := len(lines)
+	width := len(lines[0])
+	grid := NewGrid(width, height)
+	for y, line := range lines {
+		for x, char := range line {
+			grid.Set(x, y, rune(char))
+		}
+	}
+	return grid
+}
+
+func (g *Grid) Get(x, y int) rune {
+	return g.Cells[y][x]
+}
+
+func (g *Grid) Set(x, y int, value rune) {
+	g.Cells[y][x] = value
+}
+
+func (g *Grid) Print() {
+	for _, row := range g.Cells {
+		for _, cell := range row {
+			fmt.Printf("%s", string(cell))
+		}
+		fmt.Println()
+	}
+}
